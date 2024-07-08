1c3590a0b7f40d4b2ae05710137fe4ccd3806b55
Jack Reuter <jack.reuter@vergesense.com>
2024-07-08 01:38:16 -0400

    lots of work

---
+++ dani/dani.js
@@ -0,0 +1,277 @@
+import { PUZZLE } from "./grid.js";
+
+const GRID = PUZZLE['grid']
+const ACROSS_CLUES = PUZZLE['across']
+const DOWN_CLUES = PUZZLE['down']
+let rowMode = true
+
+function initBoard() {
+  let board = document.getElementById("game-board")
+  let clueNumber = 1;
+
+  let downNumbersAddedForColumns = []
+  for (let i = 0; i < 5; i++) {
+    let acrossNumberAdded = false
+    let row = document.createElement("div")
+    row.className = "letter-row"
+    row.dataset.i = i
+
+    for (let j = 0; j < 5; j++) {
+      let numberAdded = false
+      let box = document.createElement("div")
+      box.className = "letter-box"
+      box.dataset.j = j
+
+      if (GRID[i][j] === '0') {
+        box.classList.add("black-box")
+      } else {
+        if (!acrossNumberAdded) {
+          const numberDiv = document.createElement("div")
+          numberDiv.className = "box-number"
+          numberDiv.textContent = clueNumber
+          box.appendChild(numberDiv)
+          acrossNumberAdded = true
+          numberAdded = true
+          clueNumber++
+        }
+
+        if (!downNumbersAddedForColumns.includes(j)) {
+          if (!numberAdded) {
+            const numberDiv = document.createElement("div")
+            numberDiv.className = "box-number"
+            numberDiv.textContent = clueNumber
+            box.appendChild(numberDiv)
+            clueNumber++
+          }
+          downNumbersAddedForColumns.push(j)
+        }
+
+        if (i === 0) { box.classList.add("top-row") }
+        if (j === 0) { box.classList.add("left-column") }
+
+        if (i === 0) {
+          if (j === 0) {
+            box.classList.add("primary-selection")
+          } else {
+            box.classList.add("secondary-selection")
+          }
+        }
+
+        const letterSpan = document.createElement("span");
+        letterSpan.className = "letter-content";
+        box.appendChild(letterSpan);
+
+        box.addEventListener("click", () => {
+          if (box.classList.contains("primary-selection")) rowMode = !rowMode
+          setSelectedBox(i, j)
+        })
+      }
+
+      row.appendChild(box)
+    }
+
+    board.appendChild(row)
+  }
+}
+
+function initClue() {
+  let clueDiv = document.getElementById("current-clue")
+
+  clueDiv.addEventListener("click", () => {
+    let primarySelection = document.getElementsByClassName("primary-selection")[0]
+    let i = parseInt(primarySelection.parentNode.dataset.i)
+    let j = parseInt(primarySelection.dataset.j)
+
+    rowMode = !rowMode
+    setSelectedBox(i, j)
+    displayClue()
+  })
+
+  displayClue()
+}
+
+initBoard()
+initClue()
+
+document.addEventListener("keyup", (e) => {
+  let pressedKey = String(e.key)
+  if (pressedKey === "Backspace") {
+      deleteLetter()
+      return
+  }
+
+  if (pressedKey === "Enter") {
+      checkGuess()
+      return
+  }
+
+  let found = pressedKey.match(/[a-z]/gi)
+  if (!found || found.length > 1) {
+      return
+  } else {
+      insertLetter(pressedKey)
+  }
+})
+
+function insertLetter (pressedKey) {
+  pressedKey = pressedKey.toLowerCase()
+
+  let box = document.getElementsByClassName("primary-selection")[0]
+  let letterSpan = box.querySelector(".letter-content")
+  letterSpan.textContent = pressedKey;
+  selectNextBox()
+}
+
+function deleteLetter () {
+  let primarySelection = document.getElementsByClassName("primary-selection")[0]
+  let letterSpan = primarySelection.querySelector(".letter-content")
+
+  if (letterSpan.textContent !== "") {
+    letterSpan.textContent = ""
+  } else {
+    let [previousi, previousj] = previousBox()
+    setSelectedBox(previousi, previousj)
+  }
+}
+
+function selectNextBox() {
+  let [nexti, nextj] = nextBox()
+  setSelectedBox(nexti, nextj)
+}
+
+function nextBox() {
+  let box = document.getElementsByClassName("primary-selection")[0]
+  let nexti = parseInt(box.parentNode.dataset.i)
+  let nextj = parseInt(box.dataset.j)
+
+  if (rowMode) {
+    nextj++
+
+    if (nextj === 5 || GRID[nexti][nextj] === '0') {
+      nexti++
+      nextj = 0
+    }
+
+    if (nexti === 5) {
+      nexti = 0
+    }
+
+    while(GRID[nexti][nextj] === '0') {
+      nextj++
+    }
+  } else {
+    nexti++
+
+    if (nexti === 5 || GRID[nexti][nextj] === '0') {
+      nextj++
+      nexti = 0
+    }
+
+    if (nextj === 5) {
+      nextj = 0
+    }
+
+    while(GRID[nexti][nextj] === '0') {
+      nexti = (nexti + 1) % 5
+    }
+  }
+
+  return [nexti, nextj]
+}
+
+function previousBox() {
+  let box = document.getElementsByClassName("primary-selection")[0]
+  let previousi = parseInt(box.parentNode.dataset.i)
+  let previousj = parseInt(box.dataset.j)
+
+  if (rowMode) {
+    previousj--
+
+    if (previousj === -1 || GRID[previousi][previousj] === '0') {
+      previousi--
+      previousj = 4
+    }
+
+    if (previousi === -1) {
+      previousi = 4
+    }
+
+    while(GRID[previousi][previousj] === '0') {
+      previousj--
+    }
+  } else {
+    previousi--
+
+    if (previousi === -1 || GRID[previousi][previousj] === '0') {
+      previousj--
+      previousi = 4
+    }
+
+    if (previousj === -1) {
+      previousj = 4
+    }
+
+    while(GRID[previousi][previousj] === '0') {
+      previousi = (previousi - 1) % 5
+    }
+  }
+
+  return [previousi, previousj]
+}
+ 
+function setSelectedBox(newi, newj) {
+  let primary = document.getElementsByClassName("primary-selection")[0]
+  primary.classList.remove("primary-selection")
+  let secondaries = document.getElementsByClassName("secondary-selection")
+  Array.from(secondaries).forEach((box) => box.classList.remove('secondary-selection'))
+
+  let newPrimaryRow = document.querySelector(`.letter-row[data-i='${newi}']`)
+  let newPrimaryBox = newPrimaryRow.querySelector(`.letter-box[data-j='${newj}']`) 
+  newPrimaryBox.classList.add("primary-selection")
+
+  if (rowMode) {
+    for (let j=0; j<5; j++) {
+      if (newj !== j && GRID[newi][j] !== '0') {
+        let newSecondaryBox = newPrimaryRow.querySelector(`.letter-box[data-j='${j}']`)
+        newSecondaryBox.classList.add("secondary-selection")
+      }
+    }
+  } else {
+    for (let i=0; i<5; i++) {
+      if (newi !== i && GRID[i][newj] !== '0') {
+        let newSecondaryRow = document.querySelector(`.letter-row[data-i='${i}']`)
+        let newSecondaryBox = newSecondaryRow.querySelector(`.letter-box[data-j='${newj}']`)
+        newSecondaryBox.classList.add("secondary-selection")
+      }
+    }
+  }
+  displayClue()
+}
+
+function displayClue() {
+  const clueText = document.getElementById('clue-text')
+  const primaryBox = document.getElementsByClassName("primary-selection")[0]
+  const i = parseInt(primaryBox.parentNode.dataset.i)
+  const j = parseInt(primaryBox.dataset.j)
+
+  if (rowMode) {
+    clueText.textContent = ACROSS_CLUES[i] || ''
+  } else {
+    clueText.textContent = DOWN_CLUES[j] || ''
+  }
+}
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+  const target = e.target
+  
+  if (!target.classList.contains("keyboard-button")) {
+      return
+  }
+  let key = target.textContent
+
+  if (key === "Del") {
+      key = "Backspace"
+  } 
+
+  document.dispatchEvent(new KeyboardEvent("keyup", {'key': key}))
+})
---
+++ dani/grid.js
@@ -0,0 +1,23 @@
+export const PUZZLE = {
+  'grid': [
+    ['S','M','O','O','J'],
+    ['I','N','T','R','O'],
+    ['R','O','H','A','N'],
+    ['0','P','E','T','E'],
+    ['0','0','R','E','S']
+  ],
+  'across': [
+    '1. Certain spiked smoothie',
+    '6. Lead-in, casually',
+    '7. Fictional grassland known as the Riddermark',
+    '8. "Stinky" villain in Toy Story 2',
+    '9. Fancy dinner req., often'
+  ],
+  'down': [
+    '1. Title for Cristan Cole or Elton John (Brit. spelling)',
+    '2. L follower',
+    '3. Prefix with worldly',
+    '4. Something a teacher or marriage officiant may do',
+    '5. Where "we out to"'
+  ]
+}
