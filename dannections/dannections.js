78485f34017d13daea5feb91668bc82bd81cc457
Jack Reuter <jack.reuter@vergesense.com>
2024-07-13 01:02:27 -0400

    connections work

--- dannections/dannections.js
+++ dannections/dannections.js
@@ -2,10 +2,11 @@ import { PUZZLE } from "./puzzle.js"
 
 const WORDS = PUZZLE['words']
 let remainingWords = WORDS
+let guesses = []
 
 function initBoard() {
   let board = document.getElementById("game-board")
-  let rows = remainingWords.length / 4
+  let rows = Math.ceil(remainingWords.length / 4)
   let index = 0
 
   for (let i = 0; i < rows; i++) {
@@ -14,16 +15,38 @@ function initBoard() {
     row.dataset.i = i
 
     for (let j = 0; j < 4; j++) {
+      if (index >= WORDS.length) break
+
       let box = document.createElement("div")
       box.className = "box"
       box.dataset.j = j
       box.textContent = WORDS[index]
+      box.addEventListener("click", () => handleBoxClick(box))
       row.appendChild(box)
       index++
     }
 
     board.appendChild(row)
   }
+}
+
+function handleBoxClick(box) {
+  let word = box.textContent
+  if (guesses.includes(word)) {
+    box.style.backgroundColor = ""
+    guesses = guesses.filter(guess => guess !== word)
+  } else {
+    if (guesses.length >= 4) return
+    box.style.backgroundColor = "aqua"
+    guesses.push(word)
+  }
+
+  updateSubmitButtonState()
+}
+
+function updateSubmitButtonState() {
+  let submitButton = document.getElementsByClassName("submit-button")[0]
+  submitButton.disabled = guesses.length !== 4
 }
 
 initBoard()
